6cb7f9ef04cc718fc87b141960f1ea31d1f8ae13
Claimundefine <35274216+Claimundefine@users.noreply.github.com>
2024-02-25 15:36:09 -0500

    Time: 114 ms (94.09%), Space: 20.5 MB (92.34%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -5,13 +5,13 @@ class Solution:
         ans = []
         
         i = 0
-
+        
         while i < len(intervals):
             curInterval = intervals[i]
             i += 1
             while i < len(intervals) and intervals[i][0] <= curInterval[1]:
-                curInterval[1] = max(intervals[i][1], curInterval[1])
+                curInterval[1] = max(curInterval[1], intervals[i][1])
                 i += 1
             ans.append(curInterval)
-
+            
         return ans
