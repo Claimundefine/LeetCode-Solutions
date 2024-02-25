26f8bbb48a08a5c7f37551b451e9799fcc72ff13
Claimundefine <35274216+Claimundefine@users.noreply.github.com>
2024-02-25 14:41:39 -0500

    Time: 128 ms (41.82%), Space: 20.4 MB (97.42%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,17 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda x:x[0])
+
+        ans = []
+        
+        i = 0
+
+        while i < len(intervals):
+            curInterval = intervals[i]
+            i += 1
+            while i < len(intervals) and intervals[i][0] <= curInterval[1]:
+                curInterval[1] = max(intervals[i][1], curInterval[1])
+                i += 1
+            ans.append(curInterval)
+
+        return ans
