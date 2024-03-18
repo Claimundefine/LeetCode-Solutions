a76c437e2d24eba673a389b921162eb0ace4f74f
Claimundefine <35274216+Claimundefine@users.noreply.github.com>
2024-03-18 05:19:57 -0400

    Time: 1185 ms (10.79%), Space: 62.7 MB (64.62%) - LeetHub

---
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        points.sort()
+        print(points)
+        
+        count = 0
+        
+        curPoint = 0
+        
+        while curPoint < len(points):
+            rightMost = points[curPoint][1]
+            curPoint += 1
+            while curPoint < len(points) and points[curPoint][0] <= rightMost:
+                rightMost = min(rightMost, points[curPoint][1])
+                curPoint += 1
+            count += 1
+            
+        return count
+                    
