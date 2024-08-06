662f1bb8e5a862c58683cee46ecdd4b6bcfcce13
Justin Wang <35274216+Claimundefine@users.noreply.github.com>
2024-08-05 20:56:45 -0400

    Time: 243 ms (16.94%), Space: 17.6 MB (73.03%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        hashmap = {}
+        
+        for char in word:
+            if char not in hashmap:
+                hashmap[char] = 0
+            hashmap[char] += 1
+            
+        heap = []
+        
+        for key in hashmap:
+            heap.append(-1 * hashmap[key])
+            
+        heapq.heapify(heap)
+        print(heap)
+        
+        press = 0
+        letters = 0
+        
+        while heap:
+            curNum = heapq.heappop(heap)
+            press += (letters // 8  + 1) * curNum * -1
+            letters += 1
+            
+        return press
