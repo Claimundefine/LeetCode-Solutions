ce3156c74865adb9fbf9a3b1964989daae4eb4b8
Justin Wang <35274216+Claimundefine@users.noreply.github.com>
2024-08-08 10:19:14 -0400

    Time: 93 ms (16.02%), Space: 18.1 MB (9.77%) - LeetHub

---
+++ 0885-spiral-matrix-iii/0885-spiral-matrix-iii.py
@@ -0,0 +1,61 @@
+class Solution:
+    def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
+        
+        matrix = [[0] * cols for _ in range(rows)]
+        
+        matrix[rStart][cStart] = 1
+        
+        curR, curC = rStart, cStart
+        curVal = 2
+
+        numMove = 0
+        
+        numShift = 1
+        
+        direction = "L"
+        
+        ans = []
+        ans.append([curR, curC])
+        
+        while curVal <= rows * cols:
+            numMove += 1
+            if direction == "L":
+                curC += 1
+                if curR in range(rows) and curC in range(cols):
+                    matrix[curR][curC] = curVal
+                    ans.append([curR, curC])
+                    curVal += 1
+                if numMove == numShift:
+                    direction = "D"
+                    numMove = 0
+            elif direction == "D":
+                curR += 1
+                if curR in range(rows) and curC in range(cols):
+                    matrix[curR][curC] = curVal
+                    ans.append([curR, curC])
+                    curVal += 1
+                if numMove == numShift:
+                    direction = "R"
+                    numShift += 1
+                    numMove = 0
+            elif direction == "R":
+                curC -= 1
+                if curR in range(rows) and curC in range(cols):
+                    matrix[curR][curC] = curVal
+                    ans.append([curR, curC])
+                    curVal += 1
+                if numMove == numShift:
+                    direction = "U"
+                    numMove = 0
+            else:
+                curR -= 1
+                if curR in range(rows) and curC in range(cols):
+                    matrix[curR][curC] = curVal
+                    ans.append([curR, curC])
+                    curVal += 1
+                if numMove == numShift:
+                    direction = "L"
+                    numShift += 1
+                    numMove = 0
+                    
+        return ans
