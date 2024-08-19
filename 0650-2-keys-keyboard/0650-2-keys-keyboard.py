e2c445f13df843bd0b27fd46bfd67c5a6e9314e3
Justin Wang <35274216+Claimundefine@users.noreply.github.com>
2024-08-18 20:13:04 -0400

    Time: 344 ms (22.09%), Space: 16.7 MB (32.80%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minSteps(self, n: int) -> int:
+        if n == 1:
+            return 0
+        dp = [x for x in range(n+1)]
+        
+        dp[1] = 1
+        
+        for i in range(2, n+1):
+            for j in range(1, i//2 + 1):
+                if i % j == 0:
+                    print(i, j, i // j)
+                    divisor = i // j
+                    dp[i] = min(dp[i], dp[j] + divisor)
+        print(dp)
+        return dp[-1]
+                
